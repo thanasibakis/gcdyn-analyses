6455d83e2b8792d5edf3dbf729d06c00031e6fab
Thanasi Bakis <abakis@uci.edu>
2024-10-16 14:34:53 -0700

    Updated visualizations

--- simulation-studies/visualize-samples.R
+++ simulation-studies/visualize-samples.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
 
 library(tidyverse)
+library(glue)
 
 out_path <- commandArgs(trailingOnly = TRUE)[1]
-posterior_samples <- read_csv(file.path(out_path, "posterior-samples.csv"))
+posterior_samples <- read_csv(file.path(out_path, "posterior-samples.csv")) |>
+    mutate(treeset = glue("Posterior (treeset {treeset})"))
 
 prior_densities <- list(
     `φ[1]` = \(x) dlnorm(x, 0.5, 0.75),
@@ -33,72 +35,16 @@ truth <- tribble(
     "δ", 20
 )
 
-# Plot posterior median sigmoid sampling distribution
+# Setup for sigmoid plotting
 
-X <- seq(0, 10, 0.1)
-TYPE_SPACE <- c(2, 4, 6, 8)
+X <- seq(-3, 3, 0.05)
+TYPE_SPACE <- c(-2.4270176906430416, -1.4399117849363843, -0.6588015552361666, -0.13202968692343608, 0.08165101396850624, 0.7981793588605735, 1.3526378568771724, 2.1758707012574643)
 
-sigmoid <- \(x, φ1, φ2, φ3, φ4) φ1 / (1 + exp(-φ2 * (x - φ3))) + φ4
+sigmoid <- function(x, φ1, φ2, φ3, φ4) φ1 / (1 + exp(-φ2 * (x - φ3))) + φ4
 
-posterior_median_λ_quantiles <-
-    posterior_samples |>
-    summarize(
-        across(starts_with("φ"), median),
-        .by = treeset
-    ) |>
-    expand_grid(x = X) |>
-    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
-    summarise(
-        q05 = quantile(λ, 0.05),
-        q10 = quantile(λ, 0.1),
-        q20 = quantile(λ, 0.2),
-        q30 = quantile(λ, 0.3),
-        q40 = quantile(λ, 0.4),
-        q60 = quantile(λ, 0.6),
-        q70 = quantile(λ, 0.7),
-        q80 = quantile(λ, 0.8),
-        q90 = quantile(λ, 0.9),
-        q95 = quantile(λ, 0.95),
-        .by = x
-    ) |>
-    mutate(Dist = "Posterior medians")
-
-prior_λ_quantiles <-
-    map(
-        truth$Parameter,
-        \(param) tibble(
-            Parameter = param,
-            q05 = prior_quantiles[[param]](0.05),
-            q10 = prior_quantiles[[param]](0.1),
-            q20 = prior_quantiles[[param]](0.2),
-            q30 = prior_quantiles[[param]](0.3),
-            q40 = prior_quantiles[[param]](0.4),
-            q60 = prior_quantiles[[param]](0.6),
-            q70 = prior_quantiles[[param]](0.7),
-            q80 = prior_quantiles[[param]](0.8),
-            q90 = prior_quantiles[[param]](0.9),
-            q95 = prior_quantiles[[param]](0.95),
-        )
-    ) |>
-    list_rbind() |>
-    pivot_longer(
-        c(starts_with("q")),
-        names_to = "Quantile",
-        values_to = "Value"
-    ) |>
-    pivot_wider(
-        names_from = Parameter,
-        values_from = Value
-    ) |>
-    expand_grid(x = X) |>
-    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
-    select(x, λ, Quantile) |>
-    pivot_wider(names_from = Quantile, values_from = λ) |>
-    mutate(Dist = "Prior")
-
-plot_sigmoid <- function(quantiles) {
+plot_sigmoid <- function(quantiles, truth) {
     ggplot(quantiles, aes(x)) +
-        facet_wrap(vars(Dist)) +
+        facet_wrap(vars(treeset)) +
         geom_ribbon(
             aes(ymin = q05, ymax = q95),
             alpha = 0.15,
@@ -138,7 +84,7 @@ plot_sigmoid <- function(quantiles) {
         geom_vline(xintercept = TYPE_SPACE, linewidth = 1.5, linetype = "dashed") +
         scale_color_manual(values = "black") +
         labs(
-            title = "Posterior median sigmoid sampling distribution",
+            title = "Posterior sigmoid distribution",
             y = expression(lambda(x))
         ) +
         expand_limits(y = c(0, 2)) +
@@ -146,17 +92,154 @@ plot_sigmoid <- function(quantiles) {
         theme(legend.position = "bottom", legend.title = element_blank())
 }
 
+# Plot sigmoid posteriors
+
+posterior_λ_quantiles <-
+    posterior_samples |>
+    select(treeset, starts_with("φ")) |>
+    expand_grid(x = X) |>
+    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
+    summarise(
+        q05 = quantile(λ, 0.05),
+        q10 = quantile(λ, 0.1),
+        q20 = quantile(λ, 0.2),
+        q30 = quantile(λ, 0.3),
+        q40 = quantile(λ, 0.4),
+        q60 = quantile(λ, 0.6),
+        q70 = quantile(λ, 0.7),
+        q80 = quantile(λ, 0.8),
+        q90 = quantile(λ, 0.9),
+        q95 = quantile(λ, 0.95),
+        .by = c(treeset, x)
+    )
+
+prior_λ_quantiles <-
+    map(
+        truth$Parameter,
+        \(param) tibble(
+            Parameter = param,
+            q05 = prior_quantiles[[param]](0.05),
+            q10 = prior_quantiles[[param]](0.1),
+            q20 = prior_quantiles[[param]](0.2),
+            q30 = prior_quantiles[[param]](0.3),
+            q40 = prior_quantiles[[param]](0.4),
+            q60 = prior_quantiles[[param]](0.6),
+            q70 = prior_quantiles[[param]](0.7),
+            q80 = prior_quantiles[[param]](0.8),
+            q90 = prior_quantiles[[param]](0.9),
+            q95 = prior_quantiles[[param]](0.95),
+        )
+    ) |>
+    list_rbind() |>
+    pivot_longer(
+        c(starts_with("q")),
+        names_to = "Quantile",
+        values_to = "Value"
+    ) |>
+    pivot_wider(
+        names_from = Parameter,
+        values_from = Value
+    ) |>
+    expand_grid(x = X) |>
+    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
+    select(x, λ, Quantile) |>
+    pivot_wider(names_from = Quantile, values_from = λ) |>
+    mutate(treeset = "Prior")
+
 ggsave(
-    file.path(out_path, "posterior-median-sigmoids.png"),
-    bind_rows(prior_λ_quantiles, posterior_median_λ_quantiles) |> plot_sigmoid(),
+    file.path(out_path, "posterior-sigmoids.png"),
+    plot_sigmoid(
+        bind_rows(prior_λ_quantiles, posterior_λ_quantiles),
+        truth
+    ),
     width = 15,
     height = 12,
     dpi = 300
 )
 
 ggsave(
-    file.path(out_path, "posterior-median-sigmoids-no-prior.png"),
-    posterior_median_λ_quantiles |> plot_sigmoid(),
+    file.path(out_path, "posterior-sigmoids-no-prior.png"),
+    plot_sigmoid(posterior_λ_quantiles, truth),
+    width = 15,
+    height = 12,
+    dpi = 300
+)
+
+# Plot net rate and rate ratio posteriors
+
+posterior_net_rate_quantiles <-
+    posterior_samples |>
+    mutate(`φ[4]` = `φ[4]` - μ) |>
+    select(treeset, starts_with("φ")) |>
+    expand_grid(x = X) |>
+    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
+    summarise(
+        q05 = quantile(λ, 0.05),
+        q10 = quantile(λ, 0.1),
+        q20 = quantile(λ, 0.2),
+        q30 = quantile(λ, 0.3),
+        q40 = quantile(λ, 0.4),
+        q60 = quantile(λ, 0.6),
+        q70 = quantile(λ, 0.7),
+        q80 = quantile(λ, 0.8),
+        q90 = quantile(λ, 0.9),
+        q95 = quantile(λ, 0.95),
+        .by = c(treeset, x)
+    )
+
+posterior_rate_ratio_quantiles <-
+    posterior_samples |>
+    mutate(
+        `φ[1]` = `φ[1]` / μ,
+        `φ[4]` = `φ[4]` / μ
+    ) |>
+    select(treeset, starts_with("φ")) |>
+    expand_grid(x = X) |>
+    mutate(λ = sigmoid(x, `φ[1]`, `φ[2]`, `φ[3]`, `φ[4]`)) |>
+    summarise(
+        q05 = quantile(λ, 0.05),
+        q10 = quantile(λ, 0.1),
+        q20 = quantile(λ, 0.2),
+        q30 = quantile(λ, 0.3),
+        q40 = quantile(λ, 0.4),
+        q60 = quantile(λ, 0.6),
+        q70 = quantile(λ, 0.7),
+        q80 = quantile(λ, 0.8),
+        q90 = quantile(λ, 0.9),
+        q95 = quantile(λ, 0.95),
+        .by = c(treeset, x)
+    )
+
+ggsave(
+    file.path(out_path, "posterior-net-rates.png"),
+    plot_sigmoid(
+        posterior_net_rate_quantiles,
+        mutate(
+            truth,
+            Truth = case_when(
+                Parameter == "φ[4]" ~ Truth[Parameter == "φ[4]"] - Truth[Parameter == "μ"],
+                TRUE ~ Truth
+            )
+        )
+    ),
+    width = 15,
+    height = 12,
+    dpi = 300
+)
+
+ggsave(
+    file.path(out_path, "posterior-rate-ratios.png"),
+    plot_sigmoid(
+        posterior_rate_ratio_quantiles,
+        mutate(
+            truth,
+            Truth = case_when(
+                Parameter == "φ[1]" ~ Truth[Parameter == "φ[1]"] / Truth[Parameter == "μ"],
+                Parameter == "φ[4]" ~ Truth[Parameter == "φ[4]"] / Truth[Parameter == "μ"],
+                TRUE ~ Truth
+            )
+        )
+    ),
     width = 15,
     height = 12,
     dpi = 300
@@ -164,58 +247,50 @@ ggsave(
 
 # Plot histograms
 
-posterior_summaries <- posterior_samples |>
-    pivot_longer(
-        c(starts_with("φ"), μ),
-        names_to = "Parameter",
-        values_to = "Sample"
-    ) |>
-    summarise(
-        Median = median(Sample),
-        Q_025 = quantile(Sample, 0.025),
-        Q_975 = quantile(Sample, 0.975),
-        CI_Length = Q_975 - Q_025,
-        .by = c(treeset, Parameter)
-    )
+# posterior_summaries <- posterior_samples |>
+#     pivot_longer(
+#         c(starts_with("φ"), μ),
+#         names_to = "Parameter",
+#         values_to = "Sample"
+#     ) |>
+#     summarise(
+#         Median = median(Sample),
+#         Q_025 = quantile(Sample, 0.025),
+#         Q_975 = quantile(Sample, 0.975),
+#         CI_Length = Q_975 - Q_025,
+#         .by = c(treeset, Parameter)
+#     )
 
-for (parameter in truth$Parameter) {
-    p <- ggplot() +
-        stat_function(
-            aes(fill = "Prior"),
-            fun = prior_densities[[parameter]],
-            geom = "area",
-        ) +
-        geom_histogram(
-            aes(Median, after_stat(density), fill = "Posterior medians"),
-            data = filter(posterior_summaries, Parameter == parameter),
-            alpha = 0.5
-        ) +
-        geom_vline(
-            xintercept = with(truth, Truth[Parameter == parameter]),
-            color = "black",
-            linewidth = 1.5
-        ) +
-        scale_fill_manual(
-            values = c("Prior" = "grey", "Posterior medians" = "dodgerblue4")
-        ) +
-        expand_limits(x = c(0, 3)) +
-        theme_bw(base_size = 16) +
-        theme(legend.position = "bottom", legend.title = element_blank()) +
-        labs(title = "Posterior median sampling distribution", x = "Parameter")
+# for (parameter in truth$Parameter) {
+#     p <- ggplot() +
+#         stat_function(
+#             aes(fill = "Prior"),
+#             fun = prior_densities[[parameter]],
+#             geom = "area",
+#         ) +
+#         geom_histogram(
+#             aes(Median, after_stat(density), fill = "Posterior medians"),
+#             data = filter(posterior_summaries, Parameter == parameter),
+#             alpha = 0.5
+#         ) +
+#         geom_vline(
+#             xintercept = with(truth, Truth[Parameter == parameter]),
+#             color = "black",
+#             linewidth = 1.5
+#         ) +
+#         scale_fill_manual(
+#             values = c("Prior" = "grey", "Posterior medians" = "dodgerblue4")
+#         ) +
+#         expand_limits(x = c(0, 3)) +
+#         theme_bw(base_size = 16) +
+#         theme(legend.position = "bottom", legend.title = element_blank()) +
+#         labs(title = "Posterior median sampling distribution", x = "Parameter")
 
-    ggsave(
-        file.path(out_path, paste0("posterior-medians-", parameter, ".png")),
-        p,
-        width = 15,
-        height = 8,
-        dpi = 300
-    )
-}
-
-# Export coverage proportions
-
-posterior_summaries |>
-    full_join(truth, by = "Parameter") |>
-    group_by(Parameter) |>
-    summarise(Coverage = mean((Truth >= Q_025) & (Truth <= Q_975))) |>
-    write_tsv(file.path(out_path, "ci95-coverage-proportions.tsv"))
+#     ggsave(
+#         file.path(out_path, paste0("posterior-medians-", parameter, ".png")),
+#         p,
+#         width = 15,
+#         height = 8,
+#         dpi = 300
+#     )
+# }
