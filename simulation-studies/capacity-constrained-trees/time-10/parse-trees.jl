d5a46bc7789c9b54eb6bffa1dc56482146c86735
Thanasi Bakis <abakis@uci.edu>
2024-11-18 14:13:59 -0800

    Check carrying capacity time

--- simulation-studies/capacity-constrained-trees/time-10/parse-trees.jl
+++ simulation-studies/capacity-constrained-trees/time-10/parse-trees.jl
@@ -3,14 +3,19 @@ import JSON
 
 EVENT_MAPPING = Dict(
 	"birth" => :birth,
+	"death" => :unsampled_death,
 	"mutation" => :type_change,
 	"sampling" => :sampled_survival,
+	"survival" => :unsampled_survival,
 	"root" => :root
 )
 
 function main()
 	trees = map(TreeNode, JSON.parsefile("trees.json"))
 	save_object("trees.jld2", trees)
+
+	trees = map(TreeNode, JSON.parsefile("trees-unpruned.json"))
+	save_object("trees-unpruned.jld2", trees)
 end
 
 function gcdyn.TreeNode(json_tree::Dict)
